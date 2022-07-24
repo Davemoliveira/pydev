251751dfc48890a2bd056996b2d2268fbf6af283
djoliveira <davi.jorge@gmail.com>
2022-07-24 08:51:30 +0100

    V:240722

--- 001.py
+++ 001.py
@@ -1,5 +1,39 @@
 #!/bin/usr/bin/python3
 # Introduction to Python development
-var = '100'
-print('String', var)
+
+# print('comment'.upper())
+# print('comment\non-another-line')                 
+# print("1: {} 2: {}".format("Joe","Doe")) 
+
+# name = 'Dave'
+# var = ['val1', 'val2', 'val3']
+# a = 1; b = 2; c = 3.0
+# x = y = z = "var value"                            
+# print(c, 'is of type: ', type(c))
+# name += " The rave" #Adding a new value to the variable
+
+list = [1, 3, 5, 7, 9]
+# print(list[0]) # Will pull from list index (position 0)
+# print('List: [2] value = ', list[2])
+# print('Length: ',len(list))
+print('Slice 0-2: ', list[0:2]) #Slicing - will return from index 0 to 2 (2 does not return)
+print('Non-Step values: ', list[::2]) #Slicing - [start:end:step] > Will return every item except the step value| supports -1 to reverse a list
+
+
+x = (3, 'abc', 1+3)       # TUPPLE
+print('x[1] =', x[2])
+
+#MATH
+# print(pow(2, 3)) # 2 Power of 3 = 2x2x2
+# print(str(7.0)) # Pass numeric to string
+
+#BOOLEANS
+
+
+
+
+
+
+
+
 
